d14d2cea0bd55281485914d1fe1ff675e0097bc1
artkulak <artkulak444@gmail.com>
2023-08-31 17:14:41 +0000

    update react sdk

--- src/hooks/conversation.ts
+++ src/hooks/conversation.ts
@@ -387,12 +387,12 @@ export const useConversation = (
       recorderToUse = RecordRTC(audioStream, {
         type: 'audio',
         // mimeType: 'audio/wav',
-        sampleRate: micSettings.sampleRate,
+        //  sampleRate: micSettings.sampleRate,
         recorderType: StereoAudioRecorder,
         // numberOfAudioChannels: 1,
         timeSlice: timeSlice,
         // desiredSampRate: micSettings.sampleRate,
-        // bufferSize: DEFAULT_CHUNK_SIZE,
+        bufferSize: DEFAULT_CHUNK_SIZE,
         // getNativeBlob: true,
         ondataavailable: recordingDataListener
       });
