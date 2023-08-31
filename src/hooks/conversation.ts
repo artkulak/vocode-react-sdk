4f28e0b52283c7bd8d78c605ef78cd55073fc58b
artkulak <artkulak444@gmail.com>
2023-08-31 16:56:33 +0000

    update react sdk

--- src/hooks/conversation.ts
+++ src/hooks/conversation.ts
@@ -72,12 +72,12 @@ export const useConversation = (
   }, [socket]);
 
   const recordingDataListener = (data) => { // TODO: { data }: { data: Blob }
-    var a = document.createElement("a");
-    document.body.appendChild(a);
-    // a.style = "display: none";
-    a.href = window.URL.createObjectURL(data);
-    a.download = "test.wav";
-    a.click();
+    // var a = document.createElement("a");
+    // document.body.appendChild(a);
+    // // a.style = "display: none";
+    // a.href = window.URL.createObjectURL(data);
+    // a.download = "test.wav";
+    // a.click();
     blobToBase64(data).then((base64Encoded: string | null) => {
       if (!base64Encoded) return;
       const audioMessage: AudioMessage = {
