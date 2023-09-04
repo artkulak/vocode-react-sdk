9bb54e1015b1da868625cd5daa7b1463e1cacb7b
artkulak <artkulak444@gmail.com>
2023-09-04 17:24:30 +0000

    update react sdk

--- src/hooks/conversation.ts
+++ src/hooks/conversation.ts
@@ -239,6 +239,7 @@ export const useConversation = (
     };
     socket.onmessage = (event) => {
       const message = JSON.parse(event.data);
+      console.log(message)
       if (message.type === "websocket_audio") {
         setAudioQueue((prev) => [...prev, Buffer.from(message.data, "base64")]);
       } else if (message.type === "websocket_ready") {
